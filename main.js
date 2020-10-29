2a2d70f42d77a2fc43d4086c3bd5fe2de00056a5
Anna Sasińska <sasinska.anna0413@gmail.com>
2020-10-29 23:27:47 +0100

    image update & new image style

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const firstName = 'Ania';
+const firstName = "Ania";
 const age = 25;
 
 console.log(`Hej, mam na umię ${firstName} i mam ${age}`);
+
+let secondName = "Sasińska";
+secondName = "Golańska";
+
+console.log(`${secondName}`);
 
