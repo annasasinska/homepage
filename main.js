bc9350ec0b8fadb88465f2516674ccb5bd142668
Anna Sasińska <sasinska.anna0413@gmail.com>
2020-10-31 21:06:26 +0100

    querySelector

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ secondName = "Golańska";
 
 console.log(`${secondName}`);
 
+const heading = document.querySelector('.main-heading--js');
+
+console.log(heading.innerHTML);
+
+heading.innerHTML = `Hej, mam na umię ${firstName} i mam ${age}`
+
+console.log(heading.innerHTML);
